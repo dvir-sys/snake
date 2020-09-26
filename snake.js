d8b05226e50dd4f54854bcc933fdafadbea0f60f
Dvir <dvirleonardo28@gmail.com>
2020-09-26 23:55:29 +0300

    4 functions for 4 4 directions.

--- snake.js
+++ snake.js
@@ -2,12 +2,20 @@ const container = document.getElementById('container');
 const square = document.getElementById('square');  
 square.style.left = (Math.floor(Math.random() * (container.offsetWidth-square.offsetWidth)) + 1)+ 'px'; 
 square.style.top = (Math.floor(Math.random() * (container.offsetHeight-square.offsetHeight)) + 1) + 'px';
+const apple = document.getElementById('apple');
+apple.style.left = (Math.floor(Math.random() * (container.offsetWidth-apple.offsetWidth)) + 1)+ 'px'; 
+apple.style.top = (Math.floor(Math.random() * (container.offsetHeight-apple.offsetHeight)) + 1) + 'px';
+var game_end = false;
 var move_blocks = 1;
 var current_direction = 37;
 var square_left = (Math.floor(Math.random() * (container.offsetWidth-square.offsetWidth)) + 1);
 var square_top = (Math.floor(Math.random() * (container.offsetHeight-square.offsetHeight)) + 1);
-square.offsetTop
 var current_interval = setInterval(function(){
+                                                
+                                                if(parseInt(square.style.left) <= parseInt('0px') || parseInt(square.style.left) >= parseInt((container.offsetWidth-square.offsetWidth) +'px') || parseInt(square.style.top) <= parseInt('0px') || parseInt(square.style.top) >= parseInt((container.offsetHeight-square.offsetHeight)+'px')){
+                                                    alert('Game over !');
+                                                    return
+                                                }
                                                 var square_LeftPos = square.offsetLeft;
                                                 square.style.left = (square_LeftPos + move_blocks) + 'px';
 
@@ -15,14 +23,23 @@ var current_interval = setInterval(function(){
 
 
 
+function game_over(){
+    if(parseInt(square.style.left) <= parseInt('0px') || parseInt(square.style.left) >= parseInt((container.offsetWidth-square.offsetWidth) +'px') || parseInt(square.style.top) <= parseInt('0px') || parseInt(square.style.top) >= parseInt((container.offsetHeight-square.offsetHeight)+'px')){
+        alert('Game over !');
+        game_end = true;
+    }
+}
 
 
+function change_direction(e){
 
-function anim(e){
+    
     if(e.keyCode == 37 && current_direction != 37 && current_direction != 39){//left
         clearInterval(current_interval);
         current_direction = 37;
         current_interval =  setInterval(function(){
+            game_over();
+            if(game_end == true) return;                   
             var square_LeftPos = square.offsetLeft;
             square.style.left = (square_LeftPos - move_blocks) + 'px';
         
@@ -33,6 +50,8 @@ function anim(e){
         clearInterval(current_interval);
         current_direction = 39;
         current_interval =  setInterval(function(){
+                                game_over();
+                                if(game_end == true) return;
                                 var square_LeftPos = square.offsetLeft;
                                 square.style.left = (square_LeftPos + move_blocks) + 'px';
                             
@@ -43,6 +62,8 @@ function anim(e){
         clearInterval(current_interval);
         current_direction = 38;
         current_interval =  setInterval(function(){
+                                game_over();
+                                if(game_end == true) return;
                                 var square_TopPos = square.offsetTop;
                                 square.style.top = (square_TopPos - move_blocks) + 'px';
                             
@@ -53,6 +74,8 @@ function anim(e){
         clearInterval(current_interval);
         current_direction = 40;
         current_interval =  setInterval(function(){
+                                game_over();
+                                if(game_end == true) return;
                                 var square_TopPos = square.offsetTop;
                                 square.style.top = (square_TopPos + move_blocks) + 'px';
                             
@@ -61,7 +84,7 @@ function anim(e){
 
 }
 
-document.onkeydown = anim;
+document.onkeydown = change_direction;
 
 
 
